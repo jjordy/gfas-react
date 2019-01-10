68af4944f274f3ec6f6a500f7c71b70bf7dbba58
Jordan Addison <jordanrileyaddison@gmail.com>
2019-01-10 07:33:46 -0600

    fixes

--- packages/layout/src/Textarea/index.js
+++ packages/layout/src/Textarea/index.js
@@ -2,9 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 import Flex from '../Flex'
 import FormLabel from '../FormLabel'
+import { borderRadiusMixin } from '../mixins'
 
 const StyledTextarea = styled.textarea`
-  border-radius: 6px;
+  ${borderRadiusMixin};
   border: ${props =>
     props.error ? `2px solid ${props.theme.colors.red}` : '1px solid #e7e7e7'};
   margin-bottom: 1rem;
